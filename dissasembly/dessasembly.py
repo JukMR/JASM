40c821c2ecb97acfab9de9b5f3cff30da10e03c9
Julian Merida <julianmr97@gmail.com>
2023-07-24 00:08:43 -0300

    Add yaml handler and abstract content into classes

---
+++ dissasembly/dessasembly.py
@@ -0,0 +1,25 @@
+import subprocess
+
+class dessasembler:
+    def __init__(self):
+        pass
+
+    def write_to_disk(self, data: str):
+        with open('dissasembled.s', 'w', encoding='utf-8') as file:
+            file.write(data)
+
+    def dessasemble_objdump(self, binary: str) -> str | None:
+        try:
+            result = subprocess.run(['objdump', '-d', binary], capture_output=True, text=True)
+
+        # Check the return code to see if the command executed successfully
+            if result.returncode == 0:
+                self.write_to_disk(result.stdout)
+            else:
+                return f"Error: {result.stderr}"  # Return the error message, if any
+
+
+        except FileNotFoundError:
+            return "Error: objdump command not found. Make sure you have 'objdump' installed and in your system PATH."
+
+dessasembler().dessasemble_objdump('/bin/ls')
