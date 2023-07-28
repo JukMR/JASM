e8346a880460ef53c44b050363e3f7568f71a870
Julian Merida <julian.merida@eclypsium.com>
2023-07-28 18:11:18 -0300

    Temp commit to debug pyparsing capturing first 2 command digits into hex instruction coding

--- pyparsing/binary_parser.py
+++ pyparsing/binary_parser.py
@@ -1,13 +1,15 @@
 
 import argparse
-from pyparsing import Word, Suppress, OneOrMore, ParserElement, Group, SkipTo
+from pyparsing import Word, Suppress, OneOrMore, ParserElement, Group, SkipTo, Literal, White
 from pyparsing import printables, hexnums, Optional, line_end, python_style_comment
+from pyparsing.exceptions import ParseException
 
 # Set default whitespace
 # This is done to be able to parse the end of line '\n'
 # By default pyparsing will ignore newlines and whitespaces
 i_consider_whitespaces_to_be_only = ' '
-ParserElement.setDefaultWhitespaceChars(i_consider_whitespaces_to_be_only)
+ParserElement.set_default_whitespace_chars(i_consider_whitespaces_to_be_only)
+
 
 
 # Define the grammar for the binary file
@@ -18,26 +20,28 @@ COLON = ':'
 LESS_THAN = '<'
 GREATER_THAN = '>'
 
-many_line_end = OneOrMore(line_end)
+many_line_end = Suppress(line_end[1, ...])
 
 init_section_title = "Disassembly of section .init:" + many_line_end
 text_section_title = "Disassembly of section .text:" + many_line_end
 fini_section_title = "Disassembly of section .fini:" + many_line_end
 
-label = HEX + LESS_THAN + Word(printables, exclude_chars='>') + GREATER_THAN + COLON + line_end
+label = HEX + LESS_THAN + Word(printables, exclude_chars=GREATER_THAN) + GREATER_THAN + COLON + line_end
 
 INIT = Suppress(init_section_title)
 TEXT = Suppress(text_section_title)
 FINI = Suppress(fini_section_title)
 
 commentary = python_style_comment
+TAB = Suppress(White("\t "))
 
-instruction_addr = Group(HEX + COLON)
+instruction_addr = Group(HEX + COLON) + TAB
 
-hex_coding = Group(OneOrMore(Word(hexnums, exact=2))) # TODO: fix hex_coding as its taking the first 2 digit of the command
+
+hex_coding = Group(Word(hexnums, exact=2)[1, ...] + TAB) # TODO: fix hex_coding as its taking the first 2 digit of the command
 
 command = Word(printables) # TODO: generate rules for this
-instruction_code = OneOrMore(command)
+instruction_code = command[1, ...]
 
 
 inst = (
@@ -48,8 +52,8 @@ inst = (
         + many_line_end
         )
 
-line = inst | label
-lines = OneOrMore(line)
+line = label("label") | inst ("inst")
+lines = line [1, ...]
 
 init_section = INIT + lines
 text_section = TEXT + lines
@@ -59,8 +63,8 @@ fini_section = FINI + lines
 # Parse the binary file
 parsed = ( SkipTo(init_section)
           + init_section
-          + text_section
-          + fini_section
+        #   + text_section
+        #   + fini_section
           )
 
 
@@ -71,17 +75,19 @@ def main() -> None:
 
     binary_file = args.binary
 
-    # Read the binary file
-    with open(binary_file, "r") as f:
-        binary = f.read()
 
+    # Read the binary file
+    with open(binary_file, "r", encoding='utf-8') as f:
+        binary = f.read()
+        print(binary.encode('utf-8'))
 
     result = parsed.parseString(binary)
+    print(result.as_dict())
 
     # Print the instructions with their arguments
-    print(result.as_dict())
-    # for inst in result:
-    #     print(f"The instruction is: {inst}")
+    for inst in result:
+        print(f"The parsed is: {inst}")
+
 
 
 main()
