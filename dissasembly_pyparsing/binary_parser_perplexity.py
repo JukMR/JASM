d8fd39b9b36d712d343bdcb5619faa42bac3bf44
Julian Merida <julianmr97@gmail.com>
2023-07-27 23:25:04 -0300

    Fixed pyparsing not parsing '\n' newlines. Working with small binary

--- dissasembly_pyparsing/binary_parser_perplexity.py
+++ dissasembly_pyparsing/binary_parser_perplexity.py
@@ -1,14 +1,17 @@
-from pyparsing import Word, Suppress, OneOrMore, alphanums, printables, hexnums, Optional, line_end
+from pyparsing import Word, Suppress, OneOrMore, alphanums, printables, hexnums, Optional, line_end, python_style_comment, ParserElement, Group
+
+i_consider_whitespaces_to_be_only = ' '
+ParserElement.setDefaultWhitespaceChars(i_consider_whitespaces_to_be_only)
 
 # Define the grammar for the binary file
 HEX = Word(hexnums)
-COLON = ":"
-LESS_THAN ="<"
-GREATER_THAN = ">"
+COLON = ':'
+LESS_THAN = '<'
+GREATER_THAN = '>'
 
 init_title = Suppress("Disassembly of section .init:")
 text_title = Suppress("Disassembly of section .text:")
-fini_title = Suppress("Disassembly of section.fini:")
+fini_title = Suppress("Disassembly of section .fini:")
 
 header_line = Suppress(HEX) + LESS_THAN + Word(printables, exclude_chars='>') + GREATER_THAN + COLON
 
@@ -16,13 +19,19 @@ INIT = init_title + header_line
 TEXT = text_title + header_line
 FINI = fini_title + header_line
 
+commentary = python_style_comment
 
-instruction_index = HEX + COLON
-hex_coding = OneOrMore(Word(hexnums, exact=2))
+instruction_index = Suppress(Group(HEX + COLON))
+hex_coding = Group(OneOrMore(Word(hexnums, exact=2)))
 
-command = Word(printables, exclude_chars=":")
+command = Word(printables)
 instruction_code = OneOrMore(command)
-instruction = instruction_index + hex_coding + instruction_code + line_end
+instruction = (instruction_index ("index*")
+            + hex_coding ("coding*")
+            + instruction_code ("command*")
+            + Optional(commentary) ("comment*")
+            + line_end ("line_end*")
+              )
 
 # Read the binary file
 with open("binary_data.s", "r") as f:
@@ -33,7 +42,7 @@ with open("binary_data.s", "r") as f:
 parsed = OneOrMore(instruction)
 result = parsed.parseString(binary)
 
-
 # Print the instructions with their arguments
+print(result.as_dict())
 for inst in result:
     print(f"The instruction is: {inst}")
