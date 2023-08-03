4d8acb4cf9ac40bbaa70c5eb3d2b55219acf081c
Julian Merida <julian.merida@eclypsium.com>
2023-08-03 12:53:02 -0300

    Split YamlHandler and InstructionProcessor and implement mov finding regex instruction

--- YamlHandler.py
+++ YamlHandler.py
@@ -8,10 +8,32 @@ Command: TypeAlias = List[Any] | Dict[str, Any] | str
 class YamlHandler:
     def __init__(self, yaml_pathStr: str) -> None:
         self.loaded_yaml = self.read_yaml(yaml_pathStr)
+        self.processor = InstructionProcessor()
 
     def read_yaml(self, file) -> Any:
         with open(file, 'r', encoding='utf-8') as f:
             return yaml.load(stream=f.read(), Loader=yaml.Loader)
+
+    def handle_yaml_command(self, com: Command) -> str:
+
+        if isinstance(com, dict):
+            return self.processor.process_dict(com)
+        elif isinstance(com, str):
+            return com
+        else:
+            # TODO implementar excepcion para tipos no soportados por el programa
+            raise ValueError("Command type not valid")
+
+    def produce_regex(self):
+        output_regex = ''
+        for com in self.loaded_yaml['commands']:
+            output_regex += self.handle_yaml_command(com)
+        return output_regex
+
+
+class InstructionProcessor:
+    def __init__(self) -> None:
+        pass
 
     def process_any(self, any_com: Command) -> str:
         if 'include_list' in any_com:
@@ -31,13 +53,13 @@ class YamlHandler:
             return output
 
         elif 'exclude_list' in any_com:
-            return 'exclude_list'
+            return 'exclude_list' # TODO: further develop
 
     def process_not(self, not_com: Command) -> str:
-        return 'not'
+        return r'not' # TODO: further develop
 
     def process_mov(self, mov_com: Command) -> str:
-        return 'mov'
+        return r'mov[^\|]+'
 
     def process_dict(self, com) -> str:
         match list(com.keys())[0]:
@@ -50,21 +72,6 @@ class YamlHandler:
             case _:
                 raise ValueError('Invalid YAML input.')
 
-    def handle_yaml_command(self, com: Command) -> str:
-
-        if isinstance(com, dict):
-            return self.process_dict(com)
-        elif isinstance(com, str):
-            return com
-        else:
-            # TODO implementar excepcion para tipos no soportados por el programa
-            raise ValueError("Command type not valid")
-
-    def produce_regex(self):
-        output_regex = ''
-        for com in self.loaded_yaml['commands']:
-            output_regex += self.handle_yaml_command(com)
-        return output_regex
 
 def main() -> None:
     parser = argparse.ArgumentParser()
