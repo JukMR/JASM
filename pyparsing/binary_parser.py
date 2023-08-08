d0e3fc300741c75c6f071d4c34599c9a5f193449
Julian Merida <julianmr97@gmail.com>
2023-08-07 21:44:00 -0300

    Finally fix instruction codification parsing

--- pyparsing/binary_parser.py
+++ pyparsing/binary_parser.py
@@ -1,15 +1,14 @@
 
 import argparse
-from pyparsing import Word, Suppress, OneOrMore, ParserElement, Group, SkipTo, Literal, White
-from pyparsing import printables, hexnums, Optional, line_end, python_style_comment
-from pyparsing.exceptions import ParseException
+from pyparsing import Word, Suppress, ParserElement, Group, SkipTo, Literal, Optional
+from pyparsing import printables, hexnums, line_end, python_style_comment
+
 
 # Set default whitespace
 # This is done to be able to parse the end of line '\n'
 # By default pyparsing will ignore newlines and whitespaces
 i_consider_whitespaces_to_be_only = ' '
 ParserElement.set_default_whitespace_chars(i_consider_whitespaces_to_be_only)
-
 
 
 # Define the grammar for the binary file
@@ -26,28 +25,29 @@ init_section_title = "Disassembly of section .init:" + many_line_end
 text_section_title = "Disassembly of section .text:" + many_line_end
 fini_section_title = "Disassembly of section .fini:" + many_line_end
 
-label = HEX + LESS_THAN + Word(printables, exclude_chars=GREATER_THAN) + GREATER_THAN + COLON + line_end
+label = Suppress(HEX + LESS_THAN + Word(printables, exclude_chars=GREATER_THAN) + GREATER_THAN + COLON + line_end)
 
 INIT = Suppress(init_section_title)
 TEXT = Suppress(text_section_title)
 FINI = Suppress(fini_section_title)
 
-commentary = python_style_comment
+commentary = Suppress(python_style_comment)
 
-instruction_addr = Group(HEX + COLON)
+TAB = Suppress(Literal('\t'))
+instruction_addr = Suppress(HEX + COLON) + TAB
 
 
-hex_coding = Group(Word(hexnums, exact=2)[1, ...] + SkipTo('\n')) # TODO: fix hex_coding as its taking the first 2 digit of the command
+hex_coding = Suppress(Group(Word(hexnums, exact=2)[1, ...] + Optional(TAB)))
 
-command = Word(printables) # TODO: generate rules for this
-instruction_code = command[1, ...]
+command = Word(printables, exclude_chars='#') # TODO: generate rules for this
+instruction_code = Group(command[1, ...])
 
 
 inst = (
         instruction_addr ("index*")
         + hex_coding ("coding*")
-        # + Optional(instruction_code) ("command*")
-        # + Optional(commentary) ("comment*")
+        + Optional(instruction_code) ("command*")
+        + Optional(commentary) ("comment*")
         + many_line_end
         )
 
@@ -62,8 +62,8 @@ fini_section = FINI + lines
 # Parse the binary file
 parsed = ( SkipTo(init_section)
           + init_section
-        #   + text_section
-        #   + fini_section
+          + text_section
+          + fini_section
           )
 
 
@@ -80,6 +80,8 @@ def main() -> None:
         binary = f.read()
         print(binary.encode('utf-8'))
 
+
+    parsed.parse_with_tabs()
     result = parsed.parseString(binary)
     print(result.as_dict())
 
