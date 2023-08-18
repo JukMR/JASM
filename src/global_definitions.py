fc859a7c2c648b0f91a67b6660c70b716e4bfec3
Julian Merida <julian.merida@eclypsium.com>
2023-08-18 19:01:48 -0300

    Fix all mypy errors

--- src/global_definitions.py
+++ src/global_definitions.py
@@ -1,6 +1,6 @@
 'Global definition file'
 import sys
-from typing import Any, Dict, List, TypeAlias
+from typing import Any, Dict, List, TypeAlias, Optional
 from pathlib import Path
 
 IGNORE_ARGS = r'[^\|]*\|'
@@ -10,3 +10,8 @@ MAX_PYTHON_INT = sys.maxsize * 2
 PatternDict: TypeAlias = Dict[str, Any]
 Pattern: TypeAlias = List[Any] | PatternDict
 PathStr: TypeAlias = str | Path
+
+IncludeListType: TypeAlias = Optional[List[str]]
+ExcludeListType: TypeAlias = Optional[List[str]]
+TimesType: TypeAlias = Optional[Dict[str, int]]
+OperandType: TypeAlias = Optional[Dict[str, Any]]
