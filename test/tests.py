edb74c38d51ea0ceacacf8d662691d5ac3c09062
Julian Merida <julian.merida@eclypsium.com>
2023-08-23 17:43:41 -0300

    Add binary and assembly smc. Simplify pyparsing sections rules

--- test/tests.py
+++ test/tests.py
@@ -2,6 +2,10 @@
 import pytest
 import yaml
 
+import sys
+
+sys.path.append("..")
+
 from src.logging_config import logger
 from main import match
 
@@ -12,8 +16,12 @@ def load_test_configs(file_path):
         return yaml.safe_load(file_descriptor)["test_configs"]
 
 
-def run_match_test(pattern_pathstr, assembly, expected_result):
+def run_match_test(pattern_pathstr, assembly, expected_result, binary=None):
     """Run a single match test."""
+
+    if expected_result is None and binary is None:
+        raise ValueError("Wrong error configuration. At least one argument should be given")
+
     result = match(pattern_pathstr=pattern_pathstr, assembly=assembly)
     assert result == expected_result
 
@@ -22,7 +30,8 @@ def run_match_test(pattern_pathstr, assembly, expected_result):
 def test(config):
     """Test function for all configurations in configuration.yml."""
     config_yaml = config["yaml"]
-    assembly = config["assembly"]
+    assembly = config.get("assembly", None)
+    binary = config.get("binary", None)
     expected_result = config["expected"]
     logger.info("Testing assembly: %s with pattern: %s", assembly, config_yaml)
-    run_match_test(config_yaml, assembly, expected_result)
+    run_match_test(pattern_pathstr=config_yaml, assembly=assembly, expected_result=expected_result, binary=binary)
