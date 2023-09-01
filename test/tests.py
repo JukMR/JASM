89f76ededfd7a7b888156a2ebfec4e3fb9b0c363
Julian Merida <julianmr97@gmail.com>
2023-09-01 01:47:17 -0300

    Add test for checking correct number of lines from assembly files using regex

--- test/tests.py
+++ test/tests.py
@@ -1,4 +1,5 @@
 "Main test file"
+import re
 from typing import Optional
 from pathlib import Path
 import pytest
@@ -53,7 +54,7 @@ def test_all_patterns(config):
     )
 
 
-def parse_file_and_get_number_of_lines(file: str) -> int:
+def parse_file_and_get_number_of_lines_with_pyparsing(file: str) -> int:
     """Parse file and return number of lines"""
 
     parser_implementation = Parser(parser=ParserImplementation(), disassembler=DissasembleImplementation())
@@ -66,14 +67,32 @@ def parse_file_and_get_number_of_lines(file: str) -> int:
 
 @pytest.mark.parametrize(
     "config",
+    load_test_configs(file_path="configuration.yml", yaml_config_field="test_correct_lines"),
+    ids=lambda config: config["title"],
+)
+def test_correct_number_of_lines_with_regex(config) -> None:
+    """Parse file and return number of lines"""
+
+    number_of_lines = config.get("number_of_lines")
+    assembly = config.get("assembly")
+    with open(assembly, "r", encoding="utf-8") as file_descriptor:
+        readed_file = file_descriptor.read()
+
+    matched_lines = len(re.findall(r" [\dabcdef]+:\t([\dabcdef]{2} )* *\t\w*", readed_file))
+
+    assert matched_lines == number_of_lines
+
+
+@pytest.mark.parametrize(
+    "config",
     load_test_configs(file_path="configuration.yml", yaml_config_field="test_parsing_lines"),
     ids=lambda config: config["title"],
 )
-def test_parsing_number_of_lines(config):
+def test_parsing_number_of_lines(config) -> None:
     "Test parsing number of lines for all configurations in configuration.yml."
     assembly = config.get("assembly")
     number_of_lines = config.get("number_of_lines")
     logger.info("Testing assembly number of lines: %s with pattern: %s", assembly, number_of_lines)
-    parsed_number_of_lines = parse_file_and_get_number_of_lines(assembly)
+    parsed_number_of_lines = parse_file_and_get_number_of_lines_with_pyparsing(assembly)
 
     assert number_of_lines == parsed_number_of_lines
