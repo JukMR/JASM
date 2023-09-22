f0cee844123c3072cc7eaf9f86f60814c8e7fa47
Julian Merida <julian.merida@eclypsium.com>
2023-09-22 16:40:52 -0300

    Divide test file into two

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -1,61 +1,17 @@
-"Main test file"
-import re
-from typing import Optional
-from pathlib import Path
 import pytest
-import yaml
+import re
 
-
-from src.match import perform_matching, get_instruction_observers
-from src.logging_config import logger
 from src.stringify_asm.implementations.objdump.objdump_parser import ObjdumpParser
-
-
-def load_test_configs(file_path: str | Path, yaml_config_field: str):
-    """Load test configurations from a YAML file."""
-    test_folder = Path("tests")
-    with open(test_folder / file_path, "r", encoding="utf-8") as file_descriptor:
-        return yaml.safe_load(file_descriptor)[yaml_config_field]
-
-
-def run_match_test(pattern_pathstr: str, assembly: str, expected_result: bool, binary: Optional[str]) -> None:
-    """Run a single match test."""
-
-    if not assembly and not binary:
-        raise ValueError("Wrong error configuration. At least one argument should be given")
-
-    result = perform_matching(pattern_pathstr=pattern_pathstr, assembly=assembly, binary=binary)
-    assert result == expected_result
-
-
-@pytest.mark.parametrize(
-    "config",
-    load_test_configs(file_path="configuration.yml", yaml_config_field="test_configs"),
-    ids=lambda config: config["title"],
-)
-def test_all_patterns(config):
-    """Test function for all configurations in configuration.yml."""
-    config_yaml = config["yaml"]
-    expected_result = config["expected"]
-    assembly = config.get("assembly", None)
-    binary = config.get("binary", None)
-    logger.info("Testing assembly: %s with pattern: %s", assembly, config_yaml)
-    run_match_test(
-        pattern_pathstr=config_yaml,
-        assembly=assembly,
-        expected_result=expected_result,
-        binary=binary,
-    )
+from src.logging_config import logger
+from conftest import load_test_configs
+from src.match import get_instruction_observers
 
 
 def parse_file_and_get_number_of_lines_with_pyparsing(file: str) -> int:
     """Parse file and return number of lines"""
-
     parser_implementation = ObjdumpParser(assembly_pathstr=file)
     parser_implementation.set_observers(instruction_observers=get_instruction_observers())
-
     stringify_binary = parser_implementation.parse_assembly()
-
     return stringify_binary.count("|")
 
 
@@ -66,14 +22,11 @@ def parse_file_and_get_number_of_lines_with_pyparsing(file: str) -> int:
 )
 def test_correct_number_of_lines_with_regex(config) -> None:
     """Parse file and return number of lines"""
-
     number_of_lines = config.get("number_of_lines")
     assembly = config.get("assembly")
     with open(assembly, "r", encoding="utf-8") as file_descriptor:
         readed_file = file_descriptor.read()
-
     matched_lines = len(re.findall(r" [\dabcdef]+:\t([\dabcdef]{2} )* *\t\w*", readed_file))
-
     assert matched_lines == number_of_lines
 
 
@@ -88,5 +41,4 @@ def test_parsing_number_of_lines(config) -> None:
     number_of_lines = config.get("number_of_lines")
     logger.info("Testing assembly number of lines: %s with pattern: %s", assembly, number_of_lines)
     parsed_number_of_lines = parse_file_and_get_number_of_lines_with_pyparsing(assembly)
-
     assert number_of_lines == parsed_number_of_lines
