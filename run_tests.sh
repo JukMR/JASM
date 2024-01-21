ef7532edb80e5048fe83458db412cbb854800ff3
Julian Merida <julianmr97@gmail.com>
2024-01-21 03:13:57 -0300

    Improve run_tests script

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-# pytest -k "$1" -v tests/tests.py
+set -eu
+
+ARG_1=$1
+shift
+
+# pytest -k "$ARG_1" -v tests/tests.py
 
 # Run integral tests
-pytest -n 3 -k "$1" -v
+
+pytest -n 3 -k "$ARG_1" "$@"
 
 # Run unit tests
-# pytest -n 3 -k "$1" -v tests/unit_tests.py
+# pytest -n 3 -k "$ARG_1" -v tests/unit_tests.py
