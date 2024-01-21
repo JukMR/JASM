c4db9663458908b62c5f9124fa300417abc7a8a9
Julian Merida <julianmr97@gmail.com>
2024-01-21 20:04:39 -0300

    Fix run_tests.sh both with args and no args

--- run_tests.sh
+++ run_tests.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
 set -eu
-
-ARG_1=$1
-shift
+# Check if there is at least one argument
+if [ $# -eq 0 ]; then
+    echo "No arguments provided"
+    ARG_1=""
+else
+    ARG_1="-k $1"
+    shift
+fi
 
 # pytest -k "$ARG_1" -v tests/tests.py
 
 # Run integral tests
-
-pytest -n 3 -k "$ARG_1" "$@"
+pytest -n 3 -v "$ARG_1" "$@"
 
 # Run unit tests
 # pytest -n 3 -k "$ARG_1" -v tests/unit_tests.py
