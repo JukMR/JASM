4f04a7c6245f142929f25422d33a9ae2db035f2d
Julian Merida <julian.merida@eclypsium.com>
2024-01-24 12:31:27 -0300

    Remove regex checks for which type of line I am scanning to improve performance

--- performance_lenovo2.py
+++ performance_lenovo2.py
@@ -6,9 +6,9 @@ from src.match import MasterOfPuppets
 profiler = cProfile.Profile()
 profiler.enable()
 MasterOfPuppets().perform_matching(
-    pattern_pathstr="tests/yamls/moonbounce_regex_matcher.yaml",
-    input_file="tests/binary/moonbounce.bin",
+    pattern_pathstr="tests/yamls/oren_rule_att_improved_rule.yaml",
+    input_file="tests/binary/lenovo2.bin",
     input_file_type=InputFileType.binary,
 )
 profiler.disable()
-profiler.dump_stats("profiling_results.pstat")
+profiler.dump_stats("lenovo_profiling_results.pstat")
---
+++ performance_moonbounce.py
@@ -0,0 +1,14 @@
+import cProfile
+from src.global_definitions import InputFileType
+from src.match import MasterOfPuppets
+
+
+profiler = cProfile.Profile()
+profiler.enable()
+MasterOfPuppets().perform_matching(
+    pattern_pathstr="tests/yamls/moonbounce_regex_matcher.yaml",
+    input_file="tests/binary/moonbounce.bin",
+    input_file_type=InputFileType.binary,
+)
+profiler.disable()
+profiler.dump_stats("moonbounce_profiling_results.pstat")
