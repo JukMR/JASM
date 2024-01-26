afac4011f4a0f7d10c882eab9cc7ee63e40d4fec
Julian Merida <julianmr97@gmail.com>
2024-01-26 18:17:47 -0300

    Remove all gnu_objdump_parser replaced by the manual asm parser

--- src/jasm/consumer.py
+++ src/jasm/consumer.py
@@ -1,5 +1,5 @@
 import re
-from typing import Final, List, Optional, Type
+from typing import Final, List, Optional
 
 from jasm.logging_config import logger
 from jasm.stringify_asm.abstracts.abs_observer import IConsumer, IInstructionObserver, IMatchedObserver, Instruction
