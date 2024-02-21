05b258603affbf8b4d1a1afd2c0a929478ea4f42
Julian Merida <julian.merida@eclypsium.com>
2024-02-21 00:00:41 -0300

    Improve unit tests for test_pattern_node_type_builder

--- tests/unit_tests/jasm/regex/tree_generators/test_pattern_node_type_builder.py
+++ tests/unit_tests/jasm/regex/tree_generators/test_pattern_node_type_builder.py
@@ -1,6 +1,8 @@
 import pytest
 
+from jasm.global_definitions import TimeType
 from jasm.regex.tree_generators.pattern_node import PatternNode, PatternNodeTypes
+from jasm.regex.tree_generators.pattern_node_parents_builder import PatternNodeParentsBuilder
 from jasm.regex.tree_generators.pattern_node_type_builder import PatternNodeTypeBuilder
 
 
@@ -8,7 +10,7 @@ def create_test_node(name: str, parent=None, children=None, capture_group_refere
     node = PatternNode(
         pattern_node_dict={},
         name=name,
-        times=None,
+        times=TimeType(min_times=1, max_times=1),
         children=children or [],
         pattern_node_type=None,
         parent=parent,
@@ -24,7 +26,7 @@ def create_test_node(name: str, parent=None, children=None, capture_group_refere
         ("$deref", PatternNodeTypes.deref),
         ("&capture_group", PatternNodeTypes.capture_group_reference),
         ("times", PatternNodeTypes.times),
-        ("1", PatternNodeTypes.operand),
+        ("1", PatternNodeTypes.mnemonic),
         ("$or", PatternNodeTypes.node),
     ],
 )
@@ -35,24 +37,53 @@ def test_get_type(name, expected_type):
 
 
 def test_is_ancestor_deref():
-    parent = create_test_node("$deref")
-    child = create_test_node("child", parent=parent)
-    builder = PatternNodeTypeBuilder(child)
-    assert builder.is_ancestor_deref() is True
+    child = create_test_node("child", parent=[], children=[])
+    parent = create_test_node("$deref", children=[child])
+    builder = PatternNodeTypeBuilder(parent)
+    builder.build()
+
+    assert builder.command.pattern_node_type == PatternNodeTypes.deref
+    assert builder.command.children
+    assert builder.command.children[0]
+    assert builder.command.children[0].pattern_node_type == PatternNodeTypes.mnemonic
+
+    child2 = create_test_node("child2", parent=builder.command)
+    child2_builder = PatternNodeTypeBuilder(child2)
+    assert child2_builder.is_ancestor_deref() is True
 
 
 def test_any_ancestor_is_mnemonic():
-    grandparent = create_test_node("mnemonic", capture_group_references=[])
-    parent = create_test_node("parent", parent=grandparent)
-    child = create_test_node("child", parent=parent)
-    builder = PatternNodeTypeBuilder(child)
-    assert builder.any_ancestor_is_mnemonic() is True
+    child = create_test_node("child")
+    parent = create_test_node("parent", children=[child])
+    grandparent = create_test_node("mnemonic", children=[parent])
+
+    parent_builder = PatternNodeParentsBuilder(grandparent)
+    parent_builder.build()
+
+    builder_type = PatternNodeTypeBuilder(grandparent)
+    builder_type.build()
+
+    type_builder = PatternNodeTypeBuilder(child)
+    type_builder.build()
+
+    assert type_builder.any_ancestor_is_mnemonic() is True
 
 
 def test_recursive_build():
     child = create_test_node("child")
     parent = create_test_node("$deref", children=[child])
+    grandparent = create_test_node("mnemonic", children=[parent])
+
+    parent_builder = PatternNodeParentsBuilder(grandparent)
+    parent_builder.build()
+
+    builder_type = PatternNodeTypeBuilder(grandparent)
+    builder_type.build()
+
+    type_builder = PatternNodeTypeBuilder(child)
+    type_builder.build()
     builder = PatternNodeTypeBuilder(parent)
     builder.build()
+
     assert parent.pattern_node_type == PatternNodeTypes.deref
     assert child.pattern_node_type == PatternNodeTypes.deref_property
