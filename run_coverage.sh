3e0dfc7d090e70a44b29defe738d9004b16a4046
Julian Merida <julian.merida@eclypsium.com>
2024-02-23 00:44:58 -0300

    Fix opening coverage html after test

--- run_coverage.sh
+++ run_coverage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-coverage run -m pytest . && coverage report -m && coverage html && open htmlcov/index.html
+coverage run -m pytest . && coverage report -m && coverage html && xdg-open htmlcov/index.html
