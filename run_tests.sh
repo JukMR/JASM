d4225b168a910b1f911123bb540bf4bbeae250a1
Julian Merida <julian.merida@eclypsium.com>
2024-02-25 16:49:31 -0300

    Run benchmark test by default on run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -5,38 +5,21 @@
 set -eu
 
 # Initialize arguments for benchmark save and compare
-BENCHMARK_SAVE=""
-BENCHMARK_COMPARE=""
+DISABLE_BENCHMARK=false
 
 # Check for special benchmark arguments
 while [ $# -gt 0 ]; do
     case "$1" in
-    --benchmark-save)
-        echo "Benchmark save: $2"
-        BENCHMARK_SAVE="--benchmark-save=$2"
-        ENABLE_BENCHMARK=true
-        shift 2
-        ;;
-    --benchmark-compare)
-        echo "Benchmark compare: $2"
-        BENCHMARK_COMPARE="--benchmark-compare=$2"
-        shift 2
-        ENABLE_BENCHMARK=true
+    --disable-benchmark)
+        echo "Disabling benchmark"
+        DISABLE_BENCHMARK=true
+        shift 1
         ;;
     *)
         break
         ;;
     esac
 done
-
-# Check if there is at least one argument left for -k option
-if [ $# -eq 0 ]; then
-    echo "No test selection argument provided"
-    ARG_1=""
-else
-    ARG_1="-k $1"
-    shift
-fi
 
 # Configure PYTHONPATH
 if [ -z "${PYTHONPATH:-}" ]; then
@@ -46,12 +29,12 @@ else
 fi
 
 # Check whetever ENABLE_BENCHMARK is set
-if [ -n "${ENABLE_BENCHMARK:-}" ]; then
+if [ "${DISABLE_BENCHMARK:-}" ]; then
     echo "Running tests with benchmarking"
-    pytest -vv --fail-slow 5s --enable-benchmark "$BENCHMARK_SAVE" "$BENCHMARK_COMPARE" "$ARG_1" "$@"
+    pytest -vv -x --fail-slow 15s --enable-benchmark --benchmark-save='benchmark' "$@"
 
 else
     echo "Running tests without benchmarking"
-    pytest -vv --fail-slow 5s "$ARG_1" "$@"
+    pytest -vv -x --fail-slow 5s --ignore-glob='test_benchmark_degradation' "$@"
 
 fi
--- tests/test_benchmark_baselines_degradation.py
+++ tests/test_benchmark_baselines_degradation.py
@@ -2,17 +2,18 @@ import json
 from pathlib import Path
 
 
-def test_benchmark_degradation():
+def test_benchmark_degradation() -> None:
+    """Control the tests have not degraded by more than 40% in performance. If they have, fail the test."""
 
-    baseline_json = ".benchmarks/Linux-CPython-3.11-64bit/0012_baselines.json"
+    baseline_json = get_last_baseline_file()
 
     # This is the file that we want to check for performance degradation
     current_json = get_last_benchmark_file()
-    current_json = ".benchmarks/Linux-CPython-3.11-64bit/0013_new_file.json"
-    assert check_performance_degradation(baseline_json, current_json)
+    assert check_performance_degradation(baseline_json, current_json, threshold=40.0)
 
 
-def check_performance_degradation(baseline_file: str, current_file: str, threshold: float = 22.0) -> bool:
+def check_performance_degradation(baseline_file: Path, current_file: Path, threshold: float) -> bool:
+    """Check if the performance of the current benchmark has degraded by more than the threshold. If it has, return False. Otherwise, return True."""
     with open(baseline_file, "r", encoding="utf-8") as bf, open(current_file, "r", encoding="utf-8") as cf:
         baseline_data = json.load(bf)
         current_data = json.load(cf)
@@ -30,16 +31,33 @@ def check_performance_degradation(baseline_file: str, current_file: str, thresho
     return True  # Indicates all tests passed the threshold check
 
 
-def get_last_benchmark_file() -> str:
-    # This function should return the path to the last benchmark fil
+def get_last_baseline_file() -> Path:
+    """This function should return the path to the last baseline file"""
     benchmark_directory = Path(".benchmarks")
 
     for folder in benchmark_directory.iterdir():
         if folder.is_dir():
             # Sort the files in reverse order to get the lastest file
             for file in sorted(list(folder.iterdir()), reverse=True):
-                if file.is_file() and file.suffix == ".json" and not "baseline" in file.name:
-                    print("Using benchmark file to compare against baseline:", file)
-                    return str(file)
+                if file.is_file() and file.suffix == ".json":
+                    if "baseline" in file.name:
+                        print("Baseline file is:", file)
+                        return file
+
+    raise ValueError("No baseline file found")
+
+
+def get_last_benchmark_file() -> Path:
+    """This function should return the path to the last benchmark file"""
+    benchmark_directory = Path(".benchmarks")
+
+    for folder in benchmark_directory.iterdir():
+        if folder.is_dir():
+            # Sort the files in reverse order to get the lastest file
+            for file in sorted(list(folder.iterdir()), reverse=True):
+                if file.is_file() and file.suffix == ".json":
+                    if not "baseline" in file.name:
+                        print("Using benchmark file to compare against baseline:", file)
+                        return file
 
     raise FileNotFoundError("No benchmark file found")
