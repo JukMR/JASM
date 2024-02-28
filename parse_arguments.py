b1f15aa04dc3015c7fea6a91ef1dbf1c8dad1c9e
Julian Merida <julian.merida@eclypsium.com>
2024-02-28 00:50:24 -0300

    Remove argcomplete

--- parse_arguments.py
+++ parse_arguments.py
@@ -1,9 +1,6 @@
 "Parse arguments module"
 import argparse
 
-import argcomplete
-from argcomplete.completers import FilesCompleter
-
 
 def parse_args_from_console() -> argparse.Namespace:
     "Get and parse user arguments"
@@ -31,15 +28,14 @@ def parse_args_from_console() -> argparse.Namespace:
     group = parser.add_mutually_exclusive_group(required=True)
 
     # Add the two arguments to the group
-    group.add_argument("-b", "--binary", help="Input binary for parsing", type=str).completer = FilesCompleter()  # type: ignore
-    group.add_argument("-s", "--assembly", help="Input assembly for parsing", type=str).completer = FilesCompleter()  # type: ignore
+    group.add_argument("-b", "--binary", help="Input binary for parsing", type=str)
+    group.add_argument("-s", "--assembly", help="Input assembly for parsing", type=str)
 
     # Return only a file_type argument
 
     # New argument for file paths list
     parser.add_argument("--macros", nargs="+", help="List of extra macros file to use")
 
-    argcomplete.autocomplete(parser)
     parsed_args = parser.parse_args()
 
     return parsed_args
