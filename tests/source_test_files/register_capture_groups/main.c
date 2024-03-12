d37ded871b0ee893c4178ed3efe3a8d05aadb8b1
Julian Merida <julian.merida@eclypsium.com>
2024-03-12 00:01:07 -0300

    Add register capture groups test 1

---
+++ tests/source_test_files/register_capture_groups/main.c
@@ -0,0 +1,6 @@
+int f(short int x);
+
+int fun(long int x)
+{
+    return f(x+1);
+}
---
+++ tests/source_test_files/register_capture_groups/main.s
@@ -0,0 +1,10 @@
+
+main.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <fun>:
+   0:	83 c7 01             	add    $0x1,%edi
+   3:	0f bf ff             	movswl %di,%edi
+   6:	e9 00 00 00 00       	jmp    b <fun+0xb>
