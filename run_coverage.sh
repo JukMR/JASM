11b333a4416937fcd62132d08fb17e71a50296b7
Julian Merida <julian.merida@eclypsium.com>
2024-03-15 01:25:10 -0300

    Set coverage to only src/ files

--- run_coverage.sh
+++ run_coverage.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-coverage run -m pytest . && coverage report -m && coverage html && xdg-open htmlcov/index.html
+coverage run -m pytest -vv . && coverage report -m && coverage html && xdg-open htmlcov/index.html
