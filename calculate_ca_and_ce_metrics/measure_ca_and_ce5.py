0d84b350570867b30206e4906f14ed34bda8e477
Julian Merida <julianmr97@gmail.com>
2024-05-05 02:42:59 -0300

    Fix import order

--- calculate_ca_and_ce_metrics/measure_ca_and_ce5.py
+++ calculate_ca_and_ce_metrics/measure_ca_and_ce5.py
@@ -1,7 +1,7 @@
 import ast
 import os
-from typing import Any, Dict, List, Set, Tuple
 from pathlib import Path
+from typing import Any, Dict, List, Set, Tuple
 
 
 def extract_imports(path: str) -> List[str]:
